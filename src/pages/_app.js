25b78662784a144980dca3c62f23952d23a9671f
Santeri Hannula <santeri.hannula@urbit.org>
2024-04-24 17:39:51 +0300

    Init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
+import React, { useEffect } from "react";
+import { init } from "@socialgouv/matomo-next";
+import "@/styles/fonts.css";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+const MATOMO_URL = process?.env?.NEXT_PUBLIC_MATOMO_URL || "";
+const MATOMO_SITE_ID = process?.env?.NEXT_PUBLIC_MATOMO_SITE_ID || "";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    init({
+      url: MATOMO_URL,
+      siteId: MATOMO_SITE_ID,
+    });
+  });
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
