39a50e9fa063f30316f22e7933454cbfbe60c2ad
ripa <22086553+ripperi@users.noreply.github.com>
2024-04-29 23:32:58 +0300

    Favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Head from "next/head";
 import { init } from "@socialgouv/matomo-next";
 import "@/styles/fonts.css";
 import "@/styles/globals.css";
@@ -14,7 +15,32 @@ function MyApp({ Component, pageProps }) {
     });
   });
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
